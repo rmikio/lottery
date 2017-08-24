43caa1edadb7d55cd9a74409b8d8d1286f23086d
EC2 Default User <ec2-user@ip-172-31-5-229.ec2.internal>
2017-08-24 23:54:51 +0000

    Cleaning

--- MassCash/analysislottery.m
+++ MassCash/analysislottery.m
@@ -1,1 +1,85 @@
- 
+function analysislottery(matrixA)
+
+nindex = 0;
+
+supermatrix = matrixA;
+ngames = size(supermatrix,1)
+
+% process data
+
+% most numbers sorted
+% 1 to 35
+
+freqnum = zeros(35,1);
+nums = [1:35]';
+totsize = length(nums);
+
+for i=1:size(supermatrix,1)
+	for j=1:size(supermatrix,2)
+		freqnum(supermatrix(i,j),1) = freqnum(supermatrix(i,j),1) + 1;
+	endfor
+endfor
+
+for i=1:totsize
+	freqnum2(i,1) = i;
+	freqnum2(i,2) = freqnum(i,1);
+endfor
+
+% order freqnum
+
+%hist(freqnum, nums);
+
+%hist(freqnum, nums);
+%figure
+%bar(nums,freqnum);
+
+% order freq numbers sorted - order freqnum2
+
+notcomplete = 1;
+orderfreqnum = freqnum2;
+counter = 0;
+
+while notcomplete
+	notcomplete = 0;
+	counter = counter + 1;
+	% printf("Passei %d \n",counter);
+	for i=1:totsize-1
+		if orderfreqnum(i,2) > orderfreqnum(i+1,2)
+			notcomplete = 1;	
+			aux = orderfreqnum(i,2);
+			orderfreqnum(i,2) = orderfreqnum(i+1,2);
+			orderfreqnum(i+1,2) = aux;
+			aux = orderfreqnum(i,1);
+			orderfreqnum(i,1) = orderfreqnum(i+1,1);
+			orderfreqnum(i+1,1) = aux;
+		endif
+	endfor
+endwhile
+
+% show top 10 numbers less sorted
+%figure
+%bar(orderfreqnum(1:5,2),orderfreqnum(1:5,1));
+printf("-----\n");
+printf("Frequency\tNumber - in the last %d games \n",ngames);
+printf("-----\n");
+
+%for i=1:10
+%	printf("%d\t%d\n",orderfreqnum(i,2),orderfreqnum(i,1));
+%endfor
+
+% show bottom 10 numbers less sorted (top 5 most sorted)
+%figure
+%bar(orderfreqnum(totsize-5:totsize,2),orderfreqnum(totsize-5:totsize,1));
+
+%for i=totsize-10:totsize
+%	printf("%d\t%d\n",orderfreqnum(i,2),orderfreqnum(i,1));
+%endfor
+
+
+for i=1:totsize
+	printf("%d\t%d\n",orderfreqnum(i,2),orderfreqnum(i,1));
+endfor
+
+endfunction
+
+
--- MassCash/process2.m
+++ MassCash/process2.m
@@ -1,1 +1,37 @@
- 
+clear all;
+close all;
+clc;
+
+nindex = 0;
+fid = fopen('results.csv',"r");
+	while (!feof(fid))
+		nindex = nindex + 1;
+		textline = fgetl(fid);
+		auxnumbers = strsplit(textline,",");
+		for j=1:5
+			supermatrix(nindex,j) = str2num(auxnumbers{j+1});
+		endfor
+	endwhile
+fclose(fid);
+
+% process data
+
+ntotalgames = size(supermatrix,1);
+
+analysislottery(supermatrix);
+
+
+% Last 5 games
+analysislottery(supermatrix(ntotalgames-5+1:ntotalgames,:));
+
+
+% Last 10 games
+analysislottery(supermatrix(ntotalgames-10+1:ntotalgames,:));
+
+
+% Last 20 games
+analysislottery(supermatrix(ntotalgames-20+1:ntotalgames,:));
+
+% Last 30 games
+analysislottery(supermatrix(ntotalgames-30+1:ntotalgames,:));
+
